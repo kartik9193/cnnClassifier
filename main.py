97dbe325a41671396416796caec9ce87dd274774
Kartik Nerwal <kartik9193@gmail.com>
2023-08-01 16:36:25 +0530

    Prediction & UI added

--- main.py
+++ main.py
@@ -2,46 +2,46 @@ from cnnClassifier.pipeline import DataIngestionTrainingPipeline, PrepareBaseMod
 from cnnClassifier.pipeline import ModelTrainingPipeline, EvaluationPipeline
 from cnnClassifier import logger
  
-# STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = "Data Ingestion Stage"
 
 
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     data_ingestion = DataIngestionTrainingPipeline()
-#     data_ingestion.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
-# STAGE_NAME = "Prepare Base Model   Stage"
+STAGE_NAME = "Prepare Base Model   Stage"
 
 
-# try:
-#     logger.info(f"*****************************************")
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     prepare_base_model = PrepareBaseModelTrainingPipeline()
-#     prepare_base_model .main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e 
+try:
+    logger.info(f"*****************************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model .main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
 
 
 
-# STAGE_NAME = "Training Model"
+STAGE_NAME = "Training Model"
 
 
-# try:
-#     logger.info(f"*****************************************")
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     model_trainer = ModelTrainingPipeline()
-#     model_trainer.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e 
+try:
+    logger.info(f"*****************************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
 
 
 
